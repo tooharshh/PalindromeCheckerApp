b7fe7ef7705dc1871e1c3b008c45bd87153d3f8a
tooharshhh <notsoharsh1809@gmail.com>
2026-03-03 14:40:59 +0530

    UC11

---
+++ UseCase11PalindromeCheckerApp.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class UseCase11PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Input : ");
+        String input = scanner.nextLine();
+
+        PalindromeService service = new PalindromeService();
+        boolean result = service.checkPalindrome(input);
+
+        System.out.println("Is Palindrome? : " + result);
+
+        scanner.close();
+    }
+}
+
+class PalindromeService {
+
+    public boolean checkPalindrome(String input) {
+        int start = 0;
+        int end = input.length() - 1;
+        while (start < end) {
+            if (input.charAt(start) != input.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+}
